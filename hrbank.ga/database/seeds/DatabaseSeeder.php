5a9d2bb0faa57025489b3484bcb662d81bec30b6
a. kotov <a.kotov@whitewill.ru>
2020-11-29 12:53:29 +0700

    seeds

--- hrbank.ga/database/seeds/DatabaseSeeder.php
+++ hrbank.ga/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([RolesSeeder::class, UsersSeeder::class]);
+        $this->call([RolesSeeder::class, UsersSeeder::class, StatusesSeeder::class, RequirementsSeeder::class]);
     }
 }
